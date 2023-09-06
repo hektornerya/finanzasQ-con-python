23ec36ff35aa1ef6b7a42edc05ea8e59b204a7a0
hektornerya <143223093+hektornerya@users.noreply.github.com>
2023-09-06 15:38:35 -0600

    Add files via upload

--- 01_rv_sim.py
+++ 01_rv_sim.py
@@ -10,19 +10,90 @@ import pandas as pd
 import matplotlib as mpl
 import scipy
 import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
+import scipy.stats as st
 
-degrees_freedom = 5
+#inputs
+coeff = 5
+# df in student, scale in exponential
 size = 10**6
-random_variable_type = 'student' # normal student variable
+random_variable_type = 'student' 
+#options : # normal student uniform exponential chi-squared
+decimals = 5
 
+#code
+str_title = random_variable_type
 if random_variable_type == 'normal':
     x = np.random.standard_normal(size=size)
 elif random_variable_type == 'student':
-    x = np.random.standard_t(df=degrees_freedom, size=size)
-#x = np.random.uniform(low=0.0, high=1.0, size=size)
+    x = np.random.standard_t(df=coeff, size=size)
+    str_title = str_title + ' df=' + str(coeff)
+elif random_variable_type == 'uniform':    
+    x = np.random.uniform(low=0.0, high=1.0, size=size)
+elif random_variable_type == 'exponential':    
+    x = np.random.exponential(scale=coeff, size=size)
+    str_title = str_title + ' scale=' + str(coeff)
+elif random_variable_type == 'chi-squared':
+    x = np.random.chisquare(df=coeff, size=size)
+    str_title = str_title + ' df=' + str(coeff)
 
-mpl.pyplot.figure()
+mu = np.mean(x)
+sigma = np.std(x)
+skew = skew(x)
+kurt = kurtosis(x)
+
+# test of normality: Jarque-Bera
+jb_stat = size/6 * (skew**2 + 1/4*kurt**2)
+p_value = 1 - chi2.cdf(jb_stat, df=2)
+is_normal = (p_value > 0.05) # equivalently jb < 6
+
+str_title += '\n' + 'mean=' + str(np.round(mu,decimals)) \
+    + '|' + 'volatility=' + str(np.round(sigma,decimals)) \
+    + '\n' + 'skewness=' + str(np.round(skew,decimals)) \
+    + '|' + 'kurtosis=' + str(np.round(kurt,decimals)) \
+    + '\n' + 'JB stat=' + str(np.round(jb_stat, decimals)) \
+    + '|' + 'p.value=' + str(np.round(p_value, decimals)) \
+    + '\n' + 'is_normal=' + str(is_normal)
+        
+#plot       
+plt.figure()
 plt.hist(x, bins=100)
-plt.title(random_variable_type)
+plt.title(str_title)
 plt.show()
 
+
+
+
+##########################################
+# LOOP OF JARQUE-BERA NORMALITY TEST
+##########################################
+
+n = 0
+is_normal = True
+str_title = 'normal'
+
+while is_normal and n < 500:
+    x = np.random.standard_normal(size=10**6)
+    mu = st.tmean(x)  #tmean
+    sigma = st.tstd(x) #tstd
+    skew = st.skew(x)
+    kurt = st.kurtosis(x)
+    jb_stat = size/6 * (skew**2 + 1/4*kurt**2)
+    p_value = 1 - st.chi2.cdf(jb_stat, df=2)
+    is_normal = (p_value > 0.05) #equivalently jb < 6
+    print('n=' + str(n) + '| is normal' + str(is_normal))
+    n += 1
+    
+str_title += '\n' + 'mean=' + str(np.round(mu,decimals)) \
+    + '|' + 'volatility=' + str(np.round(sigma,decimals)) \
+    + '\n' + 'skewness=' + str(np.round(skew,decimals)) \
+    + '|' + 'kurtosis=' + str(np.round(kurt,decimals)) \
+    + '\n' + 'JB stat=' + str(np.round(jb_stat, decimals)) \
+    + '|' + 'p.value=' + str(np.round(p_value, decimals)) \
+    + '\n' + 'is_normal=' + str(is_normal)    
+    
+#plot       
+plt.figure()
+plt.hist(x, bins=100)
+plt.title(str_title)
+plt.show()
