595e3b421060ce108615474c177b8851d2ff558c
hektornerya <143223093+hektornerya@users.noreply.github.com>
2023-09-13 18:01:35 -0600

    Add files via upload
    hasta el v12

--- 02_market_data.py
+++ 02_market_data.py
@@ -11,39 +11,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as st
 import importlib
+import os
 
 # import our own files and reload
 import random_variables
 importlib.reload(random_variables)
+import market_data
+importlib.reload(market_data)
 
 #inputs
-ric = 'BTC-USD'
-
 directory = 'C:\\Users\\LENOVO\\.spyder-py3\\2024-1\\data\\' #hardcode
-path = directory + ric + '.csv'
-raw_data = pd.read_csv(path)
-t = pd.DataFrame()
-t['date'] = pd.to_datetime(raw_data['Date'], dayfirst=True)
-t['close'] = raw_data['Close']
-t.sort_values(by='date', ascending=True)
-t['close_previous'] = t['close'].shift(1)
-t['return_close'] = t['close']/t['close_previous'] - 1
-t= t.dropna()
-t = t.reset_index(drop=True)
-
-inputs = random_variables.simulation_inputs()
-inputs.rv_type = ric + ' | real time' 
-#options : standard_normal normal student uniform exponential chi-squared
-inputs.decimals = 5
+ric = 'SPY'
 
 # computations
-sim = random_variables.simulator(inputs)
-sim.vector = t['return_close'].values
-sim.inputs.size = len(sim.vector)
-sim.str_title = sim.inputs.rv_type
-sim.compute_stats()
-sim.plot()
-x = sim.vector
+dist = market_data.distribution(ric)
+dist.load_timeseries()
+dist.plot_timeseries()
+dist.compute_stats()
+dist.plot_histogram()
+
+
+
+# lop to check normalitu in real distributions
+rics = []
+is_normals = []
+for file_name in os.listdir(directory):
+    print('file name= ' + file_name)
+    ric = file_name.split('.')[0]
+    if ric == 'datayfinance':
+        continue
+    #compute stats
+    dist = market_data.distribution(ric)
+    dist.load_timeseries()
+    dist.compute_stats()
+    # generate lists
+    rics.append(ric)
+    is_normals.append(dist.is_normal)
+df = pd.DataFrame()
+df['ric'] = rics
+df['is_normal'] = is_normals
+df = df.sort_values(by = 'is_normal', ascending = False)
 
 
 
